c240a198b626c515d7cdca8b3ea180c26e02d7e8
letroll <julien.quievreux@gmail.com>
2014-11-26 17:51:28 +0100

    go

---
+++ app/src/main/kotlin/fr/letroll/githubbookmarkmanager/BaseActivity.kt
@@ -0,0 +1,23 @@
+package fr.letroll.githubbookmarkmanager
+
+import android.app.Activity
+import android.os.Bundle
+import android.content.pm.ActivityInfo
+import android.view.Window
+
+/**
+ * Created by jquievreux on 26/11/14.
+ */
+
+abstract class BaseActivity : Activity(){
+
+/*    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        //setContentView(contentViewId)
+    }*/
+
+}
---
+++ app/src/main/kotlin/fr/letroll/githubbookmarkmanager/MainActivity.kt
@@ -0,0 +1,9 @@
+package fr.letroll.githubbookmarkmanager
+
+/**
+ * Created by jquievreux on 26/11/14.
+ */
+
+class MainActivity : BaseActivity(){
+
+}
